dca5fdfff4bcdaac3093c6e3788d7bac89080896
illusion <fredkydd77@gmail.com>
2023-02-10 00:57:21 +0400

    added web3.js integrations with web3-providers.js file

--- main.mjs
+++ main.mjs
@@ -6,11 +6,14 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 // import * as path from 'path';
+import Web3 from 'web3';
+import 'public/scripts/web3-provider.js';
 
 dotenv.config();
 const app = express(),
   __filename = fileURLToPath(import.meta.url),
-  __dirname = dirname(__filename);
+  __dirname = dirname(__filename),
+  web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
---
+++ public/scripts/web3-provider.js
@@ -0,0 +1,23 @@
+// web3-provider.js
+if (typeof window.ethereum !== 'undefined') {
+  window.web3 = new Web3(window.ethereum);
+  try {
+    // Request account access if needed
+    await window.ethereum.enable();
+  } catch (error) {
+    console.error("User denied account access");
+  }
+}
+// Legacy dapp browsers...
+else if (typeof window.web3 !== 'undefined') {
+  window.web3 = new Web3(window.web3.currentProvider);
+}
+// Non-dapp browsers...
+else {
+  console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+}
+
+// main.js
+// import './web3-provider.js';
+
+// rest of your code here...
