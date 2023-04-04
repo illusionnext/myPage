6b4f10510b533a282c33f19c4f79eb219086b7d9
illusion <17003685+fredkydd@users.noreply.github.com>
2023-04-04 20:35:17 +0400

    preparing to back end

--- main.mjs
+++ main.mjs
@@ -1,13 +1,13 @@
-// 'use strict';
+// "use strict";
 // npm install dotenv --save
-// import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-import * as dotenv from 'dotenv';
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-// import * as path from 'path';
-// import Web3 from 'web3';
-// import 'public/scripts/web3-provider.js';
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import * as dotenv from "dotenv";
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+// import * as path from "path";
+// import Web3 from "web3";
+// import "public/scripts/web3-provider.js";
 
 dotenv.config();
 const app = express(), __filename = fileURLToPath(import.meta.url), __dirname = dirname(__filename);
@@ -16,7 +16,7 @@ const app = express(), __filename = fileURLToPath(import.meta.url), __dirname = 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 // app.port()
 
 app.listen(process.env.PORT, console.log(`server is running on ${process.env.PORT} port 🚨`));
