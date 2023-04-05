c18b892e0a964c329ea64653edffa570fe06a571
illusion <17003685+fredkydd@users.noreply.github.com>
2023-04-05 17:02:56 +0400

    made the code readable for myself

--- nodeBack/back.mjs
+++ nodeBack/back.mjs
@@ -1,3 +1,4 @@
+"use strict";
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
@@ -8,16 +9,17 @@ const app = express();
 dotenv.config();
 
 let client, db;
-const connectToMongoDB = async () => { client = new MongoClient(process.env.databaseConnect); await client.connect(); db = client.db("illusion") };
+const connectToMongoDB = async () => { client = new MongoClient(process.env.mongoDBConnect); await client.connect(); db = client.db("illusion") };
 
-(async () => await connectToMongoDB().then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(process.env.port, () => { console.log(`Server Started ${process.env.port}`) })
-}).catch(err => { console.log(err) }).finally(async () => await console.log("Closing connection to MongoDB")))()
+(async () => await connectToMongoDB()
+  .then(() => { console.log('Connected to MongoDB 😍'), app.listen(process.env.port, () => { console.log(`Server's started at ${process.env.port} 🚨`) }); })
+  .catch((err) => { console.log(err) })
+  .finally(async () => await console.log("MongoDB connection's been ended 😞")))
+  ()
 
 app.use(express.json(), cors);
 
-export const collection = (c) => db.collection(c);
+export const collection = (x) => db.collection(x);
 
 // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 // app.use("/auth", require('./routes/auth.routes'));
